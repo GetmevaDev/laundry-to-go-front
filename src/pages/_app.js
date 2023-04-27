3cfbf6eba55a40ed0ae19b4249e5e2a975f4e7cc
GetmevaDev <devteam@getmeva.com>
2023-04-27 13:32:48 +0600

    animations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,6 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/router";
+
 import { Rubik } from "@next/font/google";
 
 import "boxicons/css/boxicons.min.css";
@@ -11,9 +14,37 @@ export const rubick = Rubik({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <main className={`${rubick.variable}`}>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial="initialState"
+          animate="animateState"
+          exit="exitState"
+          transition={{
+            duration: 0.75,
+          }}
+          variants={{
+            initialState: {
+              opacity: 0,
+              clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            },
+            animateState: {
+              opacity: 1,
+              clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            },
+            exitState: {
+              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+            },
+          }}
+          className="base-page-size"
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </main>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import { HomeScreen } from "@/components/screens";
 
 export default function Home() {
