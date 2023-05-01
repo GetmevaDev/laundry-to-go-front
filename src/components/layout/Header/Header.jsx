d45a69266e0fe6b2ab3116e577a22b44ccac5000
GetmevaDev <devteam@getmeva.com>
2023-05-02 02:54:00 +0600

    navigation

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -4,6 +4,59 @@ import { Logo, Navigation } from "@/components/ui";
 
 import styles from "./Header.module.scss";
 
+const navLinks = [
+  {
+    name: "link one",
+  },
+  {
+    name: "link two",
+    children: [
+      {
+        name: "link two sub",
+      },
+      {
+        name: "link two sub 2",
+      },
+    ],
+  },
+  {
+    name: "link three",
+    children: [
+      {
+        name: "link three sub",
+      },
+      {
+        name: "link three sub 2",
+        children: [
+          {
+            name: "deep 3",
+          },
+          {
+            name: "deep 2nd 3",
+          },
+        ],
+      },
+      {
+        name: "link three sub",
+      },
+    ],
+  },
+  {
+    name: "link four",
+  },
+  {
+    name: "link five",
+    children: [
+      {
+        name: "link five sub 1",
+      },
+      {
+        name: "link five sub 2",
+      },
+    ],
+  },
+];
+
 export const Header = () => (
   <div className={styles.header}>
     <div>
--- src/components/ui/Navigation/Navigation.jsx
+++ src/components/ui/Navigation/Navigation.jsx
@@ -43,41 +43,105 @@ export const menu = [
   },
 ];
 
+export const menuItems = [
+  { label: "Home", path: "/", subMenu: [] },
+  {
+    label: "About Us",
+    path: "/about-us",
+    subMenu: [],
+  },
+  {
+    label: "Self Service",
+    path: "/self-service",
+    subMenu: [],
+  },
+  {
+    label: "Wash & Fold",
+    path: "/wash-fold",
+    subMenu: [],
+  },
+
+  {
+    label: "Pickup & Delivery",
+    path: "/pickup-delivery",
+    subMenu: [
+      {
+        label: "Commercial Laundry Services",
+        path: "/laundry-services",
+      },
+    ],
+    svg: <i className="bx bx-chevron-down" />,
+  },
+
+  {
+    label: "Pricing",
+    path: "/pricing",
+    subMenu: [],
+  },
+  {
+    label: "Contact us",
+    path: "/contact-us",
+    subMenu: [],
+  },
+];
+
 export const Navigation = ({ className }) => {
   const [nav, setNav] = useState(false);
+  const [activeMenuItem, setActiveMenuItem] = useState(null);
+
+  const handleClick = (menuItem) => {
+    setActiveMenuItem(activeMenuItem === menuItem ? null : menuItem);
+  };
 
   const router = useRouter();
-  return (
-    <nav className={styles.navigation}>
-      <ul
-        className={classNames(
-          nav ? [styles.menu, styles.active].join(" ") : [styles.menu],
-          {},
-          [className]
-        )}
-      >
-        {menu.map((item) => (
-          <li key={item.id} className={styles.item}>
-            <Link
-              href={item.href}
-              onClick={() => setNav(!nav)}
-              className={
-                router.pathname === item.href ? styles.active : styles.link
-              }
-            >
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
 
-      <div onClick={() => setNav(!nav)} className={styles.mobile_btn}>
-        {nav ? (
-          <i className="bx bx-window-close bx-rotate-180" />
-        ) : (
-          <i className="bx bx-menu-alt-right" />
-        )}
-      </div>
-    </nav>
+  const renderSubMenu = (subMenuItems) => (
+    <ul className={styles.sub_menu}>
+      {subMenuItems.map((subMenuItem) => (
+        <li key={subMenuItem.label} className={styles.sub_menu_item}>
+          <Link href={subMenuItem.path} className={styles.sub_menu_link}>
+            {subMenuItem.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      <nav className={styles.navigation}>
+        <ul
+          className={classNames(
+            nav ? [styles.menu, styles.active].join(" ") : [styles.menu],
+            {},
+            [className]
+          )}
+        >
+          {menuItems.map((item) => (
+            <li key={item.item}>
+              <Link
+                href={item.path}
+                onClick={() => handleClick(item)}
+                className={
+                  router.pathname === item.path ? styles.active : styles.link
+                }
+              >
+                {item.label}
+              </Link>
+              <span>{item.svg && item.svg}</span>
+              {item.subMenu.length > 0 && renderSubMenu(item.subMenu)}
+            </li>
+          ))}
+        </ul>
+
+        <div onClick={() => setNav(!nav)} className={styles.mobile_btn}>
+          {nav ? (
+            <i className="bx bx-window-close bx-rotate-180" />
+          ) : (
+            <i className="bx bx-menu-alt-right" />
+          )}
+        </div>
+      </nav>
+    </div>
   );
 };
